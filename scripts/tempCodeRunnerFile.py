bf9ef8fa0f8276f73ba0cdaed2b1f33820156552
Alessandro Pinto <alessandropinto2003@gmail.com>
2025-01-13 13:03:29 +0100

    created tempCodeRunnerFile.py

--- scripts/tempCodeRunnerFile.py
+++ scripts/tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-directory_path = os.getenv("DATA_PATH") 
+import os
+import subprocess
+
+if __name__ == "__main__":
+    directory_path = os.getenv("DATA_PATH") + "scripts/"
+    if directory_path:
+        subprocess.run(["python", "scripts/data_retrival.py"])
+        subprocess.run(["python", "scripts/data_cleaning.py"])
+        subprocess.run(["python", "scripts/train_models.py"])
+        subprocess.run(["python", "scripts/evaluate_models.py"])
+    else:
+        print("DATA_PATH environment variable is not set")
