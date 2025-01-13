dfa1b411022939b4e7fae9f76cc4f8350cea4b7e
Alessandro Pinto <alessandropinto2003@gmail.com>
2025-01-13 13:23:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import os
 def main():
 
     print("Step 1: Training models...")
-    os.system("python scripts/train_models.py")
-
-    print("Step 2: Evaluating models...")
-    os.system("python scripts/evaluate_models.py")
+    os.system("python scripts/tempCodeRunnerFile.py")
 
 if __name__ == "__main__":
     main()
